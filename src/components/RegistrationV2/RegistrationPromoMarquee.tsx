143edc0b6539b12ce519628366ac1909bec4c585
Jorge Cerna <jorge.cerna@example.com>
2026-07-01 23:21:56 -0600

    fIX: ajuste en código de cupón

--- src/components/RegistrationV2/RegistrationPromoMarquee.tsx
+++ src/components/RegistrationV2/RegistrationPromoMarquee.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import styles from './RegistrationPromoMarquee.module.css';
 
-const PROMO_TEXT = '⚽ ¡Últimos días para entrar a la Manada Mundialista! Regístrate antes del 5 de julio, llévate 1 mes gratis con el cupón VAMOSEMEXICO y si México gana la copa, ¡tu membresía es gratis por un año! 🙌';
+const PROMO_TEXT = '⚽ ¡Últimos días para entrar a la Manada Mundialista! Regístrate antes del 5 de julio, llévate 1 mes gratis con el cupón VAMOSMEXICO y si México gana la copa, ¡tu membresía es gratis por un año! 🙌';
 
 export default function RegistrationPromoMarquee() {
     return (
